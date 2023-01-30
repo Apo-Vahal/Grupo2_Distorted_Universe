4eb3f5ac3bb75cb1cc448a5159dba905898ac700
DavyDaveDavid <102179116+DavyDaveDavid@users.noreply.github.com>
2023-01-30 19:38:37 +0100

    Port
    Just a Port

---
+++ Assets/Scripts/Props/Compass.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Compass : MonoBehaviour
+{
+    public Vector3 NorthDirection; //Dirección del Norte. 
+    public Transform Player; //Posición del jugador.
+    public RectTransform NorthLayer; 
+   
+    void Update()
+    {
+        ChangeNorthDirection(); 
+    }
+
+    public void ChangeNorthDirection()
+    {
+        NorthDirection.z = Player.eulerAngles.y; //La dirección del norte en el eje z (es decir, en la profundidad) será igual 
+                                                 // a la rotación en relación con el mundo del eje y del Player. 
+        NorthLayer.localEulerAngles = NorthDirection; 
+    }
+}
