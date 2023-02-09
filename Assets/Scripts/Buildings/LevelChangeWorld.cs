522de0bdf030e7227c3aa6e820b8992cce9a7cb5
NicoGlo <nicoglo2103@gmail.com>
2023-02-09 13:12:14 +0100

    Escenas
    El interior de la taberna y las ruinas estan, además de puestos en el mapa con sus correspondientes tps a las escenas.

---
+++ Assets/Scripts/Buildings/LevelChangeWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangeWorld : MonoBehaviour
+{
+    //If the player's collider collides with the object's collider, the scene will swap to the menu(in this case)
+    public string levelchange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController detectedPlayer = other.GetComponent<PlayerController>();
+        if (detectedPlayer != null)
+        {
+            SceneManager.LoadScene("Open_Wordl");
+        }
+    }
+}
